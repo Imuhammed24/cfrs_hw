2ff967c4d4c694f59e0f16dec8dce17cba981bde
imuhammed24 <ishaqmuhammed011@gmail.com>
2021-11-09 00:01:47 +0100

    trying to find data in img

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-# This is a sample Python script.
+import os
+import imghdr
+from PIL import Image
+
 
 def b64decode(name):
     return
@@ -11,9 +14,25 @@ def md5_hash_func(content):
 
 def main(content):
     # user enters directory
-    directory = input('Please enter directory: ')
+    directory_name = input('Please enter directory (C:/Users/sselt/Documents/blog_demo): ')
+    # get list of files in current dir: test: /home/ishaq/PycharmProjects/cfrs_hw
+    # path_dir_list = os.listdir(directory)
+
+    # traverse the diretory entered
+    for current_directory, child_directories, child_files in os.walk(directory_name):
+        # loop through the files in each directory traversed
+        for child_file in child_files:
+            # for each file, check if it is an image
+            image_type = imghdr.what(os.path.join(current_directory, child_file))
+            if image_type == 'jpeg' or image_type == 'JPEG':
+                # check image for appended data
+                print(current_directory, child_file)
+
+                img = Image.open(os.path.join(current_directory, child_file))
+                img_exif = img._getexif()
+                print(img_exif)
+
     return
-    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 if __name__ == '__main__':
