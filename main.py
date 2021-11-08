6f40de4e30657be6a2e70c60eb2518a33ea7da16
imuhammed24 <ishaqmuhammed011@gmail.com>
2021-11-08 18:15:31 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+def b64decode(name):
+    return
+
+
+def md5_hash_func(content):
+    return
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def main(content):
+    # user enters directory
+    directory = input('Please enter directory: ')
+    return
+    # print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    main('PyCharm')
