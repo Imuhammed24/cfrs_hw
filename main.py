ead0aa673ee19ad16f14b06b60b6969ae6c40ce2
imuhammed24 <ishaqmuhammed011@gmail.com>
2021-11-09 09:23:04 +0100

    trying to find data in img

--- main.py
+++ main.py
@@ -1,6 +1,62 @@
 import os
+import string
+from hashlib import md5
 import imghdr
-from PIL import Image
+import time
+
+import PIL
+from PIL import Image, ExifTags
+from PIL.ExifTags import TAGS
+from exif import Image as ExifImage
+
+
+def convert_image_to_ascii(image_directory):
+    import sys
+    from PIL import Image
+
+    # pass the image as command line argument
+    # image_path = sys.argv[1]
+    img = Image.open(image_directory)
+
+    # resize the image
+    # width, height = img.size
+    # aspect_ratio = height / width
+    # new_width = 120
+    # new_height = aspect_ratio * new_width * 0.55
+    # img = img.resize((new_width, int(new_height)))
+    # new size of image
+    # print(img.size)
+
+    # convert image to greyscale format
+    # img = img.convert('L')
+
+    pixels = img.getdata()
+
+    # replace each pixel with a character from array
+    chars = ["B", "S", "#", "&", "@", "$", "%", "*", "!", ":", "."]
+    new_pixels = [chars[pixel // 25] for pixel in pixels]
+    new_pixels = ''.join(new_pixels)
+
+    # split string of chars into multiple strings of length equal to new width and create a list
+    new_pixels_count = len(new_pixels)
+    ascii_image = [new_pixels[index:index + new_width] for index in range(0, new_pixels_count, new_width)]
+    ascii_image = "\n".join(ascii_image)
+    print(ascii_image, '\n\n\n\n')
+    return ascii_image
+
+
+def strings(filename, minimum=4):
+    with open(filename, errors="ignore") as f:  # Python 3.x
+        result = ""
+        for c in f.read():
+            if c in string.printable:
+                result += c
+                continue
+            if len(result) >= minimum:
+                yield result
+            result = ""
+        if len(result) >= minimum:  # catch result at EOF
+            yield result
 
 
 def b64decode(name):
@@ -16,24 +72,41 @@ def main(content):
     # user enters directory
     directory_name = input('Please enter directory (C:/Users/sselt/Documents/blog_demo): ')
     # get list of files in current dir: test: /home/ishaq/PycharmProjects/cfrs_hw
-    # path_dir_list = os.listdir(directory)
 
     # traverse the diretory entered
     for current_directory, child_directories, child_files in os.walk(directory_name):
         # loop through the files in each directory traversed
         for child_file in child_files:
             # for each file, check if it is an image
-            image_type = imghdr.what(os.path.join(current_directory, child_file))
+            try:
+                img = Image.open(os.path.join(current_directory, child_file))
+                image_type = img.format
+            except PIL.UnidentifiedImageError:
+                image_type = None
+
             if image_type == 'jpeg' or image_type == 'JPEG':
                 # check image for appended data
-                print(current_directory, child_file)
+                file1 = open(f"{child_file.split('.')[0]}.txt", "a")
 
-                img = Image.open(os.path.join(current_directory, child_file))
-                img_exif = img._getexif()
-                print(img_exif)
+                current_image_path = os.path.join(current_directory, child_file)
+                print(child_file)
+                for sting in strings(current_image_path):
+                    file1.writelines(sting)
+                file1.close()
 
+                print("Created: %s" % time.ctime(os.path.getctime(os.path.join(current_directory, child_file))))
+                # print("Last Modified: %s" % time.ctime(os.path.getmtime(os.path.join(current_directory, child_file))))
+                # print("Last Accessed: %s" % time.ctime(os.path.getatime(os.path.join(current_directory, child_file))))
+
+                img.close()
     return
 
 
 if __name__ == '__main__':
     main('PyCharm')
+
+# find ascii equiv: strings jpg01
+# identify base64
+# strings
+# echo 'RGVzaWduZWRieUZyZWVwaWs=' | base64 -d
+# sys allows to run sytem command
