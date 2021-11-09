40f7c83f19747b27d1474d59f1d6aa3b08bcd76e
imuhammed24 <ishaqmuhammed011@gmail.com>
2021-11-09 22:20:29 +0100

    - recreated image without secret message

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def main(content):
     cleaned_images_trailer = {}
     decoded_messages = {}
     all_images_timestamps = {}
-    # get list of files in current dir: test: /home/ishaq/PycharmProjects/cfrs_hw
+    images_ascii_representation = {}
 
     # traverse the directory entered
     for current_directory, child_directories, child_files in os.walk(directory_name):
@@ -57,6 +57,7 @@ def main(content):
 
             if image_type == 'jpeg' or image_type == 'JPEG':
                 # check image for appended data
+                image_has_appended_data = False
 
                 current_image_path = os.path.join(current_directory, child_file)
                 print(child_file)
@@ -79,10 +80,12 @@ def main(content):
                 if current_string_valid:
                     if len(current_image_ascii_trailer) >= 4:
                         if len(current_image_ascii_trailer) % 4 != 0:
+                            image_has_appended_data = True
                             if not cleaned_images_trailer.get(child_file.split('.')[0]):
                                 cleaned_images_trailer[child_file.split('.')[0]] = current_image_ascii_trailer[
                                                                                    len(current_image_ascii_trailer) % 4:]
                         elif len(current_image_ascii_trailer) % 4 == 0:
+                            image_has_appended_data = True
                             if not cleaned_images_trailer.get(child_file.split('.')[0]):
                                 cleaned_images_trailer[child_file.split('.')[0]] = current_image_ascii_trailer
 
@@ -92,9 +95,22 @@ def main(content):
                     'last_modified': time.ctime(os.path.getmtime(os.path.join(current_directory, child_file))),
                     'last_accessed': time.ctime(os.path.getatime(os.path.join(current_directory, child_file))),
                 }
-                # print("Created: %s" % time.ctime(os.path.getctime(os.path.join(current_directory, child_file))))
-                # print("Last Modified: %s" % time.ctime(os.path.getmtime(os.path.join(current_directory, child_file))))
-                # print("Last Accessed: %s" % time.ctime(os.path.getatime(os.path.join(current_directory, child_file))))
+
+                # save image ascii characters
+                if image_has_appended_data:
+                    images_ascii_representation[child_file.split('.')[0]] = current_image_ascii_lines[:-1]
+                else:
+                    images_ascii_representation[child_file.split('.')[0]] = current_image_ascii_lines
+
+                # create image with
+                with open(current_image_path, "rb") as file:
+                    # lines = file.readline()
+                    with open(f"{child_file.split('.')[0]}.jpg", "wb") as new_file:
+                        # check if the image has appended data
+                        if image_has_appended_data:
+                            new_file.writelines([line for line in file.readlines()[:-1]])
+                        else:
+                            new_file.writelines([line for line in file.readlines()])
 
     # get messages
     for encoded_message in cleaned_images_trailer.keys():
@@ -103,9 +119,6 @@ def main(content):
         decoded_messages[encoded_message] = decoded_message
 
     print(all_images_timestamps)
-
-
-    # get time stamps
 
     # get the modulus4
     # for test_string in strings_to_test_for_length:
